fcb87b14d240ba38534702df8eb33c7861826bd0
keb18 <spliff.star@gmail.com>
2018-05-28 17:26:32 +0100

    Generate different pages for /index, /register, /login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  path = '';
+  checkPath: boolean;
+  removeNavFrom = [
+    '',
+    '/login',
+    '/register'
+  ];
+
+  constructor(private router: Router, private location: Location) {
+    this.router.events.subscribe((val) => {
+      this.path = this.location.path();
+      // console.log(this.path);
+      this.checkPath = this.removeNavFrom.includes(this.path);
+      // console.log(this.checkPath);
+    });
+   }
 }
